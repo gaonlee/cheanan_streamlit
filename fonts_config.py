92ee1b82186849d19fe6cd09009620c04551f98c
gaonlee <leemil1211@gmail.com>
2024-08-25 15:40:33 +0900

    폰트폰트

--- fonts_config.py
+++ fonts_config.py
@@ -5,8 +5,9 @@ import os
 font_path = 'NanumSquareL.otf'
 
 def setup_fonts():
+    # 폰트 파일 경로를 수정 (프로젝트 폴더 내에 저장한 경우)
+    font_path = os.path.join(os.path.dirname(__file__), 'fonts', 'NanumSquareL.otf')
     
-    font_path = 'NanumSquareL.otf'  # 파일이 루트 디렉토리에 있으므로 이 경로로 수정
     if os.path.exists(font_path):
         font_name = fm.FontProperties(fname=font_path).get_name()
         plt.rc('font', family=font_name)
@@ -14,9 +15,8 @@ def setup_fonts():
         print(f"Font loaded: {font_name}")
     else:
         print(f"Font not found at {font_path}")
-        plt.rc('font', family='Noto Sans KR')  # 대체 폰트 설정 (한글 미지원)
-
-    # matplotlib가 다른 폰트를 찾지 않도록 명시적으로 설정
+        plt.rc('font', family='Noto Sans KR')
+        
     plt.rcParams['font.family'] = font_name
     plt.rcParams['font.sans-serif'] = [font_name]
 
