f852e979ce10a76efd905bb1f5c8ad5d3968e463
gaonlee <leemil1211@gmail.com>
2024-08-25 13:17:24 +0900

    폰트 배포

--- fonts_config.py
+++ fonts_config.py
@@ -1,11 +1,9 @@
-import os
 import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
+import os
 
 def setup_fonts():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    font_path = os.path.join(base_dir, 'fonts', 'NanumSquareL.otf')
-    
+    font_path = '/app/fonts/NanumSquareL.otf'  # Streamlit 클라우드에서의 폰트 경로
     if os.path.exists(font_path):
         font_name = fm.FontProperties(fname=font_path).get_name()
         plt.rc('font', family=font_name)
@@ -13,7 +11,6 @@ def setup_fonts():
         print(f"Font loaded: {font_name}")
     else:
         print(f"Font not found at {font_path}")
-        # 추가적으로 기본 폰트로 설정
-        plt.rc('font', family='DejaVu Sans')
+        plt.rc('font', family='DejaVu Sans')  # 폰트가 없을 경우 대체 폰트 사용
 
 setup_fonts()
