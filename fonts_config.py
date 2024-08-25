f67de98fa3424591703c869ec982eac0ea2d02d2
gaonlee <leemil1211@gmail.com>
2024-08-25 12:57:06 +0900

    폰트 로그

--- fonts_config.py
+++ fonts_config.py
@@ -10,6 +10,11 @@ def setup_fonts():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     font_path = os.path.join(base_dir, 'fonts', 'NanumSquareL.otf')
     
+    print(f"폰트 경로: {font_path}")  # 경로 확인을 위해 추가
+    
+    if not os.path.exists(font_path):
+        raise FileNotFoundError(f"폰트 파일을 찾을 수 없습니다: {font_path}")
+    
     font_name = fm.FontProperties(fname=font_path).get_name()
     plt.rc('font', family=font_name)
     plt.rcParams['axes.unicode_minus'] = False
